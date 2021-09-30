d6055493c6f34369a43a37bd531fef5922c2f558
Laura <laurabf6@gmail.com>
2021-09-30 21:27:40 +0200

    M9_Cohetes   NIVEL 1   Fase 1

---
+++ M9_Cohetes_N1/src/nivel1/Fase1/Rocket.java
@@ -0,0 +1,22 @@
+package nivel1.Fase1;
+
+public class Rocket {
+	
+	private String code;
+	private int numProps;
+
+	public Rocket(String code, int numProps) throws Exception {
+		if(code.length()!=8)
+			throw new Exception("WRONG CODE!! SHOULD BE 8 CHAR LONG");
+		if(numProps==0)
+			throw new Exception("AT LEAST ONE PROP NEEDED!!");
+		this.code = code.toUpperCase();
+		this.numProps = numProps;	
+	}
+
+	@Override
+	public String toString() {
+		return "Rocket id=" + code + ": Tiene " + numProps + " propulsores";
+	}
+	
+}
---
+++ M9_Cohetes_N1/src/nivel1/Fase1/Rockets_APP.java
@@ -0,0 +1,13 @@
+package nivel1.Fase1;
+
+public class Rockets_APP {
+
+	public static void main(String[] args) throws Exception {
+		
+		Rocket r1 = new Rocket("32WESSDS", 3);
+		Rocket r2 = new Rocket("ldsfja32", 6);
+
+		System.out.println(r1 +"\n"+ r2);
+	}
+
+}

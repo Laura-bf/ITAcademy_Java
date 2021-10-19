23d82cfbc44194d876e1d45fea72924245b3177e
Laura <laurabf6@gmail.com>
2021-10-19 11:16:02 +0200

    M11_Floristeria_PairProgramming	 NIVEL 1

---
+++ M11_Floristeria_N1/src/com/nivel1/view/utils/Session.java
@@ -0,0 +1,33 @@
+package com.nivel1.view.utils;
+
+public class Session {
+
+	private StateValue stateValue;
+	
+	public Session() {
+		this.stateValue = StateValue.MAIN_MENU;
+	}
+	
+	public StateValue getStateValue() {
+		return this.stateValue;
+	}
+	
+	public void setStateValue(StateValue stateValue) {
+		this.stateValue = stateValue;
+	}
+	
+	public void nextMenu() {
+		 this.stateValue = StateValue.values()[this.stateValue.ordinal()+1];
+	}
+	
+	public void previousMenu() {
+		 this.stateValue = StateValue.values()[this.stateValue.ordinal()-1];
+	}
+	public void floristMenu() {
+		this.stateValue = StateValue.FLORIST_MENU;
+	}
+	
+	 public void exit() {
+	        this.stateValue = StateValue.EXIT_MENU;
+	}
+}

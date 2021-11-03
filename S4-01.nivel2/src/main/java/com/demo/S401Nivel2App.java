a1cec450bb537e381ce8793a9da1f194456fc880
Laura <laurabf6@gmail.com>
2021-11-03 17:52:04 +0100

    S4.01   NIVEL 2

---
+++ S4-01.nivel2/src/main/java/com/demo/S401Nivel2App.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S401Nivel2App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S401Nivel2App.class, args);
+	}
+
+}
---
+++ S4-01.nivel2/src/test/java/com/demo/S401/nivel2/S401Nivel2AppTests.java
@@ -0,0 +1,13 @@
+package com.demo.S401.nivel2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S401Nivel2AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3dc8fbc5ae40d9594de73886ace170276fb8d7f9
Laura <laurabf6@gmail.com>
2022-01-13 14:37:03 +0100

    Tests entidades

--- S502.DiceGame/src/test/java/com/diceGame/model/domain/PlayerPersistenceTest.java
+++ S502.DiceGame/src/test/java/com/diceGame/model/domain/PlayerPersistenceTest.java
@@ -3,7 +3,6 @@ package com.diceGame.model.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,22 +12,16 @@ import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @ActiveProfiles("test")
-class PlayerTest {
+class PlayerPersistenceTest {
 
 	@Autowired
 	private TestEntityManager entityManager;
 	
 	private static Player testPlayer;
-	private static Roll roll1;
-	private static Roll roll2;
-	private static Roll roll3;
-	
+
 	@BeforeAll
 	public static void setUp() {
 		testPlayer = new Player("testName","pw");
-		roll1 = new Roll();
-		roll2 = new Roll();
-		roll3 = new Roll();
 	}
 	
 	@Test
@@ -38,7 +31,7 @@ class PlayerTest {
 	}
 	
 	@Test
-	public void createPlayerNullNameThrowsException() {
+	public void createPlayerNullNameThrowsIllegalArgumentException() {
 		try {
 			testPlayer = new Player("","pw");
 			fail("Exception Expected!");
@@ -50,21 +43,7 @@ class PlayerTest {
 		}
 	}
 
-	@Test
-	final void testCalculateRate() {
-		roll1.setWon(true);;
-		roll2.setWon(true);
-		roll3.setWon(false);
-		testPlayer.addRoll(roll1);
-		testPlayer.addRoll(roll2);
-		testPlayer.addRoll(roll3);
-		double expected = 2/3d;
-		
-		testPlayer.calculateRate();
-		double result = testPlayer.getRate();
-		
-		assertTrue(expected*100==result);
-	}
+	
 
 }
 
